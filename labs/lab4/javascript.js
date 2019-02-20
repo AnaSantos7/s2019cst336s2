287936f180c53daa0f05271a70be0ff7f0c472ef
Ana Santos <asantos@csumb.edu>
2019-02-20 21:52:51 +0000

    added lab4

--- labs/lab4/javascript.js
+++ labs/lab4/javascript.js
@@ -38,13 +38,75 @@ var myQuestions = [
 			d: "inputfile"
 		},
 		correctAnswer: 'b'
+	},
+	{
+		question: "What header file contains C++ file I/O instructions?",
+		answers: {
+			a: "iostream.h",
+			b: "fstream.h",
+			c: "infstream.h",
+			d: "outstream.h"
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "What purpose do classes serve?",
+		answers: {
+			a: "data encapsulation",
+			b: "providing a convenient way of modeling real-world objects",
+			c: "simplifying code reuse",
+			d: "all of the above"
+		},
+		correctAnswer: 'd'
+	},
+	{
+		question: "Which of the following is the proper keyword to deallocate memory?",
+		answers: {
+			a: "remove",
+			b: "clear",
+			c: "delete",
+			d: "free"
+		},
+		correctAnswer: 'c'
+	},
+	{
+		question: "Which of the following gives the memory address of integer variable a?",
+		answers: {
+			a: "*a;",
+			b: "a;",
+			c: "&a",
+			d: "address(a)"
+		},
+		correctAnswer: 'c'
+	},
+	{
+		question: "A constructor that accepts __________ parameters is called the default constructor.",
+		answers: {
+			a: "no",
+			b: "one",
+			c: "two",
+			d:	"three"
+		},
+		correctAnswer: 'a'
+	},
+	{
+		question: "Constructors __________ to allow different approaches of object construction.",
+		answers: {
+			a: "cannot be overloaded",
+			b: "can be overloaded",
+			c: "can be called",
+			d: "can be nested"
+		},
+		correctAnswer: 'b'
 	}
+
 ];
 
 
 var quizContainer = document.getElementById('quiz');
 var resultsContainer = document.getElementById('results');
 var submitButton = document.getElementById('submit');
+var message = document.getElementById('msg');
 
 generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
 
@@ -108,12 +170,15 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 		}
 
 		resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+		 if(numCorrect >= 9) {
+			message.innerHTML = "Well done! ( :";
+		}
 	}
 
 	showQuestions(questions, quizContainer);
 	
 	submitButton.onclick = function(){
 		showResults(questions, quizContainer, resultsContainer);
-	}
+	};
 
 }
